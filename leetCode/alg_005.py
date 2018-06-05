bef7c1a1978d9ea5312d2f9ae6fed902299a6339
oaimnil <miao.lin@teralytics.ch>
2018-06-05 09:54:23 +0800

    consolidate two methods

--- leetCode/alg_005.py
+++ leetCode/alg_005.py
@@ -13,7 +13,7 @@ Output: "bb"
 """
 
 
-class Solution:
+class Solution_gd:
     # greedy solution
     def longestPalindrome(self, s):
         """
@@ -42,11 +42,38 @@ class Solution:
         return s[l+1:r]
 
 
+class Solution_dp:
+    def longestPalindrome(self, s):
+
+        n = len(s)
+        results = [[False] * n for i in range(n)]
+        x, y = 0, 0
+
+        for i in range(n):
+            results[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i+1]:
+                results[i][i+1] = True
+                if not x and not y:
+                    x, y = i, i + 1
+
+        for k in range(2, n):
+            for i in range(n - 2):
+                j = i + k
+                if j == n:
+                    break
+                if results[i+1][j-1] and s[i] == s[j]:
+                    results[i][j] = True
+
+                    if j - i > y - x:
+                        x, y = i, j
+        return s[x:y+1]
 
 
 if __name__ == "__main__":
 
-    test = Solution()
+    test = Solution_dp()
 
     s = "babad"
     print(test.longestPalindrome(s))
